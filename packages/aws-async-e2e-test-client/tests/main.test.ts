76884fc39aa74100c860f96fe84ecbc8d9717da4
yamatatsu <the.kinnikuman@gmail.com>
2024-03-04 19:09:44 +0900

    init

---
+++ packages/aws-async-e2e-test-client/tests/main.test.ts
@@ -0,0 +1,26 @@
+import {
+	CloudWatchLogs,
+	FilterLogEventsCommand,
+} from "@aws-sdk/client-cloudwatch-logs";
+import { mockClient } from "aws-sdk-client-mock";
+import { expect, test } from "vitest";
+
+const logs = new CloudWatchLogs();
+const logsMock = mockClient(CloudWatchLogs);
+
+test("should pass", async () => {
+	logsMock
+		.on(FilterLogEventsCommand)
+		.resolvesOnce({ events: [] })
+		.resolvesOnce({ events: [] })
+		.resolvesOnce({
+			events: [{ timestamp: 0, message: "" }],
+		});
+
+	let res = await logs.filterLogEvents({ logGroupName: "test" });
+	expect(res).toEqual({ events: [] });
+	res = await logs.filterLogEvents({ logGroupName: "test" });
+	expect(res).toEqual({ events: [] });
+	res = await logs.filterLogEvents({ logGroupName: "test" });
+	expect(res).toEqual({ events: [{ timestamp: 0, message: "" }] });
+});
